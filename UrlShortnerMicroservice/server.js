ff74365c7e01e4959db288dc7bba3d5e31922d01
Sepideh <sepidehfallah2@gmail.com>
2019-10-01 13:03:48 -0700

    Creating a UI for the microservice

--- UrlShortnerMicroservice/server.js
+++ UrlShortnerMicroservice/server.js
@@ -7,6 +7,15 @@ app.use(bodyParser.json());
 // http://expressjs.com/en/starter/static-files.html
 // app.use(express.static("public"));
 
+/** this project needs to parse POST bodies **/
+// mount the body-parser here
+
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
 app.post("/api/shorturl/new", function(req, res) {
   console.log("I ammmm here!");
 });
