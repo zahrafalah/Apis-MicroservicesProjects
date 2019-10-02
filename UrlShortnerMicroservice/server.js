f7a11cf9354eb2e5a5bc01a6dfdc4db327326b33
Sepideh <sepidehfallah2@gmail.com>
2019-10-01 17:37:28 -0700

    Api get request will create the dabase entries and return json object with original url and the id of the entry

--- UrlShortnerMicroservice/server.js
+++ UrlShortnerMicroservice/server.js
@@ -1,5 +1,11 @@
+//Get requirment and set instance of them
 var express = require("express");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var shortUrl = require("./models/shortUrl.js");
+
+//connect to our database
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/shortUrls");
 
 var app = (module.exports = express());
 app.use(bodyParser.json());
@@ -10,16 +16,45 @@ app.use(bodyParser.json());
 /** this project needs to parse POST bodies **/
 // mount the body-parser here
 
+//Allows node to find static content
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/api/shorturl/new", function(req, res) {
+//Create the database entry
+app.get("/new/:urlToShorten", (req, res) => {
+  var { urlToShorten } = req.params;
+  //test regular expressions
+  var regex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+  if (regex.test(urlToShorten)) {
+    const data = new shortUrl({
+      originalUrl: urlToShorten,
+      shorterUrl: shortUrl.id
+    });
+
+    data.save(err => {
+      if (err) {
+        return res.send("Error occured while saving data to database");
+      }
+    });
+    return res.json(data);
+  }
+  var data = new shortUrl({
+    originalUrl: "Invalid URL",
+    shorterUrl: shortUrl.id
+  });
+  return res.json(data);
+});
+
+//Query database and return the originalUrl
+
+app.post("/new", (req, res) => {
   console.log("I ammmm here!");
 });
 
-app.listen(3000, function() {
+//process here means if it is on heroku
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000!");
 });
